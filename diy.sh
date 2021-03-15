bac1fd303f5f235c01d10bc028eeb365eae18c3d
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-03-15 20:46:15 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -18,7 +18,7 @@ scripts_base_url_1=https://github.com/acoolbook/scripts/blob/main/
 
 ##############################  作  者  脚  本  名  称  （必填）  ##############################
 # 将相应作者的脚本填写到以下变量中
-my_scripts_list_1="jd_entertainment.js jd_shakeBean.js jd_axc.js jd_xxl_gh.js jd_factory_component.js jd_marketLottery.js jd_superDay.js jd_xmf.js jd_wish.js jd_nc.js jd_jxproducts_detail.js"
+my_scripts_list_1="jd_entertainment.js jd_shakeBean.js jd_axc.js jd_xxl_gh.js jd_factory_component.js jd_marketLottery.js jd_superDay.js jd_xmf.js jd_wish.js jx_nc.js jx_products_detail.js"
 
 ## 活动脚本名称1：百变大咖秀、摇京豆、东东爱消除、个护爱消除、京喜工厂Plus、京东超市-大转盘、超级品类日
 
@@ -89,5 +89,5 @@ sed -i "s|bash jd jd_marketLottery|bash ${ShellDir}/jd.sh jd_marketLottery|g" ${
 sed -i "s|bash jd jd_superDay|bash ${ShellDir}/jd.sh jd_superDay|g" ${ListCron}
 sed -i "s|bash jd jd_xmf|bash ${ShellDir}/jd.sh jd_xmf|g" ${ListCron}
 sed -i "s|bash jd jd_wish|bash ${ShellDir}/jd.sh jd_wish|g" ${ListCron}
-sed -i "s|bash jd jd_nc|bash ${ShellDir}/jd.sh jd_nc|g" ${ListCron}
-sed -i "s|bash jd jd_jxproducts_detail.js|bash ${ShellDir}/jd.sh jd_jxproducts_detail.js|g" ${ListCron}
+sed -i "s|bash jd jx_nc|bash ${ShellDir}/jd.sh jx_nc|g" ${ListCron}
+sed -i "s|bash jd jx_products_detail.js|bash ${ShellDir}/jd.sh jx_products_detail.js|g" ${ListCron}
