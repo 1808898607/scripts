ed3cd071398bbdd848b9eef61ecd63ee3c94014b
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-03-15 20:20:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -4,7 +4,7 @@
 
 ##############################  作  者  昵  称  （必填）  ##############################
 # 使用空格隔开
-author_list="SuperManito"
+author_list="acoolbook"
 
 ## 目前使用本人收集的脚本库
 
@@ -12,7 +12,7 @@ author_list="SuperManito"
 # 例如：https://raw.sevencdn.com/whyour/hundun/master/quanx/jx_nc.js
 # 1.从作者库中随意挑选一个脚本地址，每个作者的地址添加一个即可，无须重复添加
 # 2.将地址最后的 “脚本名称+后缀” 剪切到下一个变量里（my_scripts_list_xxx）
-scripts_base_url_1=https://gitee.com/SuperManito/scripts/raw/master/
+scripts_base_url_1=https://github.com/acoolbook/scripts/blob/main/
 
 ## 添加更多脚本地址URL示例：scripts_base_url_3=https://raw.sevencdn.com/SuperManito/JD-FreeFuck/main/
 
