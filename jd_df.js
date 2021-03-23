d79b367108bc29c73017625235c0c7af76bf35f0
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-03-23 11:55:37 +0800

    Update jd_df.js

--- jd_df.js
+++ jd_df.js
@@ -45,22 +45,19 @@ let tuanActiveId = `6S9y4sJUfA2vPQP6TLdVIQ==`;
 const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://wqsd.jd.com/pingou/dream_factory/index.html%22%20%7D`;
 let cookiesArr = [], cookie = '', message = '', allMessage = '';
 const inviteCodes = [
-  'cR6akRvMKEZDQo6d_N7boA==@wotQXakCeOom5OFicq29hw==@2G935SZ1n3J2-57oYsnjow==@Aor7O2L5WyXS4_oQTOdd5A==',
-  'cR6akRvMKEZDQo6d_N7boA==@wotQXakCeOom5OFicq29hw==@2G935SZ1n3J2-57oYsnjow==@Aor7O2L5WyXS4_oQTOdd5A==',
-  'cR6akRvMKEZDQo6d_N7boA==@wotQXakCeOom5OFicq29hw==@2G935SZ1n3J2-57oYsnjow==@Aor7O2L5WyXS4_oQTOdd5A==',
-  'cR6akRvMKEZDQo6d_N7boA==@wotQXakCeOom5OFicq29hw==@2G935SZ1n3J2-57oYsnjow==@Aor7O2L5WyXS4_oQTOdd5A==',
-  'cR6akRvMKEZDQo6d_N7boA==@wotQXakCeOom5OFicq29hw==@2G935SZ1n3J2-57oYsnjow==@Aor7O2L5WyXS4_oQTOdd5A==',
-  'cR6akRvMKEZDQo6d_N7boA==@wotQXakCeOom5OFicq29hw==@2G935SZ1n3J2-57oYsnjow==@Aor7O2L5WyXS4_oQTOdd5A==',
-  'cR6akRvMKEZDQo6d_N7boA==@wotQXakCeOom5OFicq29hw==@2G935SZ1n3J2-57oYsnjow==@Aor7O2L5WyXS4_oQTOdd5A==',
+  'V5LkjP4WRyjeCKR9VRwcRX0bBuTz7MEK0-E99EJ7u0k=@0WtCMPNq7jekehT6d3AbFw==',
+  "gB99tYLjvPcEFloDgamoBw==@7dluIKQMp0bySgcr8AqFgw==",
+  '-OvElMzqeyeGBWazWYjI1Q==',
+  'GFwo6PntxDHH95ZRzZ5uAg=='
 ];
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 $.tuanIds = [];
 /*
 url签名参数
  */
-const random = 'H6/aVJK6Ikxh';
-let token = `tk01wcb0f1dbfa8ncCs3RVJTWmROsQFiQEFr0SFKwv1UscY0AzsNkcnhPppzDHItv6SZ0/BerRpNTjivoMuzlCWtgtel`;
-let fingerprint = 3220710875724161;
+const random = 'cNlpbJCwIFx/';
+let token = `tk01wc7951ceea8nVzY0UlBvK3QvPAfwG6UuVMo3YIwnuyPtwgIZr9BSCkJT96NMHRqNWCO5x0zbNsEA2bkjst3tYymV`;
+let fingerprint = 6318883301648161;
 const appId = 10001;
 
 let fingerprintJD = '';
@@ -1660,7 +1657,7 @@ function decrypt(time, stk, type, url) {
       hash1 = enCryptMethodJD(urlSignTokenJD, fingerprintJD, timestamp, appId, $.CryptoJS);
     } else {
       const str = `${token}${fingerprint}${timestamp}${appId}${random}`;
-      hash1 = $.CryptoJS.HmacMD5(str, token);
+      hash1 = $.CryptoJS.HmacSHA512(str, token);
     }
     let st = '';
     stk.split(',').map((item, index) => {
