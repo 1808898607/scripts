c961cbd09afd1a0919b0747d28f79d19ebe03bde
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-04-09 13:15:36 +0800

    Update jd_mc.js

--- jd_mc.js
+++ jd_mc.js
@@ -12,13 +12,7 @@
 [task_local]
 #有机牧场
 0 0,1-22/2 * 4-7 * https://raw.githubusercontent.com/monk-coder/dust/dust/member/monk_pasture.js, tag=有机牧场,  enabled=true
-================Loon==============
-[Script]
-cron "0 */1 * 4-7 *" script-path=https://raw.githubusercontent.com/monk-coder/dust/dust/member/monk_pasture.js,tag=有机牧场
-===============Surge=================
-#有机牧场 = type=cron,cronexp="0 0,1-22/2 1-31 4-7 *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/monk-coder/dust/dust/member/monk_pasture.js
-============小火箭=========
-#有机牧场 = type=cron,script-path=https://raw.githubusercontent.com/monk-coder/dust/dust/member/monk_pasture.js, cronexpr="0 0,1-22/2 1-31 4-7 *", timeout=3600, enable=true
+// cron "0 */2 * 4-7 *" script-path=https://raw.githubusercontent.com/monk-coder/dust/dust/i-chenzhe/z_mother_jump.js,tag=母婴-跳一跳
 */
 const $ = new Env('有机牧场');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
