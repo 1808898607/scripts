7213e87259527cc87beac71735a54461055f2ff2
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-04-11 12:02:13 +0800

    Update jd_entertainment.js

--- jd_entertainment.js
+++ jd_entertainment.js
@@ -15,10 +15,10 @@
 ============Quantumultx===============
 [task_local]
 #百变大咖秀
-10 10,11 * * 2-5 https://raw.githubusercontent.com/monk-coder/dust/dust/i-chenzhe/z_entertainment.js, tag=百变大咖秀,  enabled=true
+10 10,11 * * 4 https://raw.githubusercontent.com/monk-coder/dust/dust/i-chenzhe/z_entertainment.js, tag=百变大咖秀,  enabled=true
 ================Loon==============
 [Script]
-cron "10 7,11 * * 2-5" script-path=https://github.com/acoolbook/scripts/edit/main/jd_entertainment.js,tag=百变大咖秀
+cron "10 7,11 * 4 *" script-path=https://github.com/acoolbook/scripts/edit/main/jd_entertainment.js,tag=百变大咖秀
 ===============Surge=================
 百变大咖秀 = type=cron,cronexp="10 10,11 * * 2-5",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/monk-coder/dust/dust/i-chenzhe/z_entertainment.js
 ============小火箭=========
