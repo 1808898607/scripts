ef0d2be1db42e4fa65b2a3a35e40dc5922d88700
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-04-12 14:52:48 +0800

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -19,6 +19,11 @@ function sj(){
 echo "6 0,6,23 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_jxnc.js >> /scripts/logs/jd_jxnc.log 2>&1" >> /scripts/docker/merged_list_file.sh
 echo "45 23 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_cash.js >> /scripts/logs/jd_cash.log 2>&1" >> /scripts/docker/merged_list_file.sh
 echo "10 0 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_jdzz.js >> /scripts/logs/jd_jdzz.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "59 23 * * * sleep 58; node conc /scripts/sj_jd_jmc.js >> /scripts/logs/jd_mc.log 2>&1" >> /scripts/docker/merged_list_file.s
+echo "59 23 * * * sleep 57; node conc /scripts/sj_jd_jmc.js >> /scripts/logs/jd_mc.log 2>&1" >> /scripts/docker/merged_list_file.s
+echo "59 23 * * * sleep 59; node conc /scripts/sj_jd_jmc.js >> /scripts/logs/jd_mc.log 2>&1" >> /scripts/docker/merged_list_file.s
+echo "0 0 * * * sleep 2; node conc /scripts/sj_jd_jmc.js >> /scripts/logs/jd_mc.log 2>&1" >> /scripts/docker/merged_list_file.s
+echo "0 0 * * * sleep 4; node conc /scripts/sj_jd_jmc.js >> /scripts/logs/jd_mc.log 2>&1" >> /scripts/docker/merged_list_file.s
 #echo "22 * * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_dreamFactory.js >> /scripts/logs/jd_dreamFactory.log 2>&1" >> /scripts/docker/merged_list_file.sh
 echo "0,2 0 * * * sleep 1; node conc /scripts/sj_jd_carnivalcitys.js >> /scripts/logs/jd_carnivalcity.log 2>&1" >> /scripts/docker/merged_list_file.sh
 #echo "59 7,15,23 * * * sleep 57; node conc /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_rewardbf7.log 2>&1" >> /scripts/docker/merged_list_file.sh
