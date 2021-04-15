46749aaa215caa25b39c5b677b61edcc6e77194e
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-04-16 00:03:50 +0800

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -21,16 +21,10 @@ echo "6 0,6,23 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_jxn
 echo "45 23 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_cash.js >> /scripts/logs/jd_cash.log 2>&1" >> /scripts/docker/merged_list_file.sh
 echo "12 0 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_jdzz.js >> /scripts/logs/jd_jdzz.log 2>&1" >> /scripts/docker/merged_list_file.sh
 echo "0 0 * * * sleep 1; node conc /scripts/jd_shop.js >> /scripts/logs/jd_shop.log 2>&1" >> /scripts/docker/merged_list_file.sh
-echo "59 23 * * * sleep 58; node conc /scripts/sj_jd_mc.js >> /scripts/logs/jd_mc.log 2>&1" >> /scripts/docker/merged_list_file.sh
-#echo "59 23 * * * sleep 57; node conc /scripts/sj_jd_mc.js >> /scripts/logs/jd_mc.log 2>&1" >> /scripts/docker/merged_list_file.sh
 echo "59 23 * * * sleep 59; node conc /scripts/sj_jd_mc.js >> /scripts/logs/jd_mc.log 2>&1" >> /scripts/docker/merged_list_file.sh
 #echo "0 0 * * * sleep 1; node conc /scripts/sj_jd_mc.js >> /scripts/logs/jd_mc.log 2>&1" >> /scripts/docker/merged_list_file.sh
-#echo "0 0 * * * sleep 4; node conc /scripts/sj_jd_mc.js >> /scripts/logs/jd_mc.log 2>&1" >> /scripts/docker/merged_list_file.sh
 echo "1 6 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_dreamFactory.js >> /scripts/logs/jd_dreamFactory.log 2>&1" >> /scripts/docker/merged_list_file.sh
 echo "0,2 0 * * * sleep 10; node conc /scripts/sj_jd_carnivalcitys.js >> /scripts/logs/jd_carnivalcity.log 2>&1" >> /scripts/docker/merged_list_file.sh
-#echo "59 7,15,23 * * * sleep 57; node conc /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_rewardbf7.log 2>&1" >> /scripts/docker/merged_list_file.sh
-#echo "59 7,15,23 * * * sleep 58; node conc /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_rewardbf8.log 2>&1" >> /scripts/docker/merged_list_file.sh
-#echo "59 7,15,23 * * * sleep 59; node conc /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_rewardbf9.log 2>&1" >> /scripts/docker/merged_list_file.sh
 
 function main(){
     # 首次运行时拷贝docker目录下文件
