7297f48c6989c5bc2575f65b786e4611d12a15f8
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-04-21 10:44:47 +0800

    Update jd_super_redrain.js

--- jd_super_redrain.js
+++ jd_super_redrain.js
@@ -23,7 +23,7 @@ let bodyList = {
 }
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
-const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+//const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
