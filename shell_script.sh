40ae29a8dcaf6a956df1a57934958d3ac5d20424
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-04-22 11:37:53 +0800

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -24,6 +24,7 @@ echo "6,17 0 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_syj.j
 echo "0 0 * * * sleep 1; node /scripts/jd_shop.js >> /scripts/logs/jd_shop.log 2>&1" >> /scripts/docker/merged_list_file.sh
 #echo "59 23 * * * sleep 59; node conc /scripts/sj_jd_mc.js >> /scripts/logs/jd_mc.log 2>&1" >> /scripts/docker/merged_list_file.sh
 echo "3 0,6 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_dreamFactory.js >> /scripts/logs/jd_dreamFactory.log 2>&1" >> /scripts/docker/merged_list_file.sh
+apk add nodejs-current
 
 function main(){
     # 首次运行时拷贝docker目录下文件
