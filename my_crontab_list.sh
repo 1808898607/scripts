737a8df03a0826cdd2d1bb728cc4148f6521dcb3
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-04-24 18:09:53 +0800

    Update my_crontab_list.sh

--- my_crontab_list.sh
+++ my_crontab_list.sh
@@ -121,4 +121,4 @@
 #TCL
 3 0,23 * * * node /scripts/jd_tcl.js >> /scripts/logs/jd_tcl.log 2>&1
 #test
-10 18 * * * sleep 10; node conc /scripts/jd_cash.js >> /scripts/logs/jd_cash.log 2>&1
+12 18 * * * sleep 10; node conc /scripts/jd_cash.js >> /scripts/logs/jd_cash.log 2>&1
