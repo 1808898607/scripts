4b761751b33bdb307e4ace41ea9dba8df4694d59
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-04-27 10:50:07 +0800

    Update jddj_fruit.js

--- jddj_fruit.js
+++ jddj_fruit.js
@@ -9,18 +9,26 @@
 const $ = new API("djgy");
 let cookies = [];
 let thiscookie = '', deviceid = '';
-let lat = Math.round(Math.random() * (35 - 30) + 30) + '.' + Math.round(Math.random() * (99999 - 10000) + 10000);
-let lng = Math.round(Math.random() * (125 - 120) + 120) + '.' + Math.round(Math.random() * (99999 - 10000) + 10000);
+let lat = '30.' + Math.round(Math.random() * (99999 - 10000) + 10000);
+let lng = '114.' + Math.round(Math.random() * (99999 - 10000) + 10000);
 let cityid = Math.round(Math.random() * (1500 - 1000) + 1000);
 !(async () => {
-    if ($.env.isNode) cookies = require('./jddj_cookie.js');
     if (cookies.length == 0) {
-        let ckstr = $.read('#jddj_cookies');
-        if (ckstr.indexOf(',') < 0) {
-            cookies.push(ckstr);
-        } else {
-            cookies = str.split(',');
+        if ($.env.isNode) { delete require.cache['./jddj_cookie.js']; cookies = require('./jddj_cookie.js') }
+        else {
+            let ckstr = $.read('#jddj_cookies');
+            if (!!ckstr) {
+                if (ckstr.indexOf(',') < 0) {
+                    cookies.push(ckstr);
+                } else {
+                    cookies = ckstr.split(',');
+                }
+            }
         }
+    }
+    if (cookies.length == 0) {
+        console.log(`\r\n请先填写cookie`);
+        return;
     }
     for (let i = 0; i < cookies.length; i++) {
         console.log(`\r\n★★★★★开始执行第${i + 1}个账号,共${cookies.length}个账号★★★★★`);
@@ -37,6 +45,13 @@ let cityid = Math.round(Math.random() * (1500 - 1000) + 1000);
         });
         deviceid = jsonlist.deviceid_pdj_jd;
 
+        var shareCode = '';
+        await userinfo();
+        await $.wait(1000);
+
+        await treeInfo();
+        await $.wait(1000);
+
         let tslist = await taskList();
         if (tslist.code == 1) {
             $.notify('第' + (i + 1) + '个账号cookie过期', ',请访问https://daojia.jd.com/html/index.html抓取cookie', { url: 'https://daojia.jd.com/html/index.html' });
@@ -47,6 +62,9 @@ let cityid = Math.round(Math.random() * (1500 - 1000) + 1000);
         await $.wait(1000);
 
         await runTask(tslist);
+        await $.wait(1000);
+
+        await zhuLi();
         await $.wait(1000);
 
         await water();
@@ -62,6 +80,29 @@ let cityid = Math.round(Math.random() * (1500 - 1000) + 1000);
 }).finally(() => {
     $.done();
 })
+
+//个人信息
+async function userinfo() {
+    return new Promise(async resolve => {
+        try {
+            let option = urlTask('https://daojia.jd.com/client?_jdrandom=' + Math.round(new Date()) + '&platCode=H5&appName=paidaojia&channel=&appVersion=8.7.6&jdDevice=&functionId=mine%2FgetUserAccountInfo&body=%7B%22refPageSource%22:%22%22,%22fromSource%22:2,%22pageSource%22:%22myinfo%22,%22ref%22:%22%22,%22ctp%22:%22myinfo%22%7D&jda=&traceId=' + deviceid + Math.round(new Date()) + '&deviceToken=' + deviceid + '&deviceId=' + deviceid + '', '')
+
+            $.http.get(option).then(response => {
+                let data = JSON.parse(response.body);
+                if (data.code == 0) {
+                    nickname = data.result.userInfo.userBaseInfo.nickName;
+                    console.log("●●●" + nickname + "●●●");
+                }
+            })
+            resolve();
+
+        } catch (error) {
+            console.log('\n【个人信息】:' + error);
+            resolve();
+        }
+    })
+}
+
 
 //任务列表
 async function taskList() {
@@ -128,7 +169,25 @@ async function sign() {
         }
 
     })
+}
 
+//助力
+async function zhuLi() {
+    return new Promise(async resolve => {
+        try {
+            let scodes = 'JD_b8b3c7d14339000@23f5861a35e48ef,JD_b8b5c4ac7d29000@23f586f456dcf50,JD_b8b85dad1f5a000@23f5870bd2f9bea,JD_b8bbd041af18000@23f58722d802053,JD_b8bcfa8f7842000@23f58739e3aec6e,JD_b8fa483e8739000@23f587514a271b2,JD_21eccc93aa0f3c5@23f587667507965'.split(',');
+            let scode = scodes[Math.round(Math.random() * (scodes.length - 1) + 0)];
+            let option = urlTask(`https://daojia.jd.com/client?lat=${lat}&lng=${lng}&lat_pos=${lat}&lng_pos=${lng}&city_id=${cityid}&deviceToken=${deviceid}&deviceId=${deviceid}&channel=wx_xcx&mpChannel=wx_xcx&platform=5.0.0&platCode=mini&appVersion=5.0.0&appName=paidaojia&deviceModel=appmodel&xcxVersion=9.2.0&isNeedDealError=true&business=djgyzhuli&functionId=task%2Ffinished&body=%7B%22modelId%22%3A%22M10007%22%2C%22taskType%22%3A1201%2C%22taskId%22%3A%2223eee1c043c01bc%22%2C%22plateCode%22%3A5%2C%22assistTargetPin%22%3A%22${scode.split('@')[0]}%22%2C%22uniqueId%22%3A%22${scode.split('@')[1]}%22%7D`, ``);
+            $.http.get(option).then(response => {
+                let data = JSON.parse(response.body);
+                //console.log('\n【助力】:' + data.msg);
+                resolve();
+            })
+        } catch (error) {
+            //console.log('\n【助力】:' + error);
+            resolve();
+        }
+    })
 }
 
 //运行任务列表
@@ -199,6 +258,7 @@ async function treeInfo() {
                 let data = JSON.parse(response.body);
                 if (data.code == 0) {
                     console.log('\n【果树信息】:' + data.result.activityInfoResponse.fruitName + ',还需浇水' + data.result.activityInfoResponse.curStageLeftProcess + '次' + data.result.activityInfoResponse.stageName + ',还剩' + data.result.userResponse.waterBalance + '滴水');
+                    shareCode = data.result.activityInfoResponse.userPin;
                 }
                 resolve();
             })
