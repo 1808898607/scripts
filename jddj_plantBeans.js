34faebffcbc0912cec8f2d30d575ad3da68af0cb
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-04-27 10:55:40 +0800

    Update jddj_plantBeans.js

--- jddj_plantBeans.js
+++ jddj_plantBeans.js
@@ -6,23 +6,21 @@
 //boxjs订阅地址:https://gitee.com/passerby-b/javascript/raw/master/JD/passerby-b.boxjs.json
 // cron "13 8,13,21 * * *" script-path=https://github.com/717785320/JDDJ/blob/main/jddj_plantBeans.js
 
-const $ = new API("jddj_bean");
+const $ = new API(" jddj_plantBeans");
 let cookies = [];
 let thiscookie = '', deviceid = '';
 let lat = '30.' + Math.round(Math.random() * (99999 - 10000) + 10000);
 let lng = '114.' + Math.round(Math.random() * (99999 - 10000) + 10000);
 let cityid = Math.round(Math.random() * (1500 - 1000) + 1000);
 !(async () => {
+    if ($.env.isNode) cookies = require('./jddj_cookie.js');
     if (cookies.length == 0) {
-        if ($.env.isNode) { delete require.cache['./jddj_cookie.js']; cookies = require('./jddj_cookie.js') }
-        else {
-            let ckstr = $.read('#jddj_cookies');
-            if (!!ckstr) {
-                if (ckstr.indexOf(',') < 0) {
-                    cookies.push(ckstr);
-                } else {
-                    cookies = ckstr.split(',');
-                }
+        let ckstr = $.read('#jddj_cookies');
+        if (!!ckstr) {
+            if (ckstr.indexOf(',') < 0) {
+                cookies.push(ckstr);
+            } else {
+                cookies = ckstr.split(',');
             }
         }
     }
@@ -31,7 +29,7 @@ let cityid = Math.round(Math.random() * (1500 - 1000) + 1000);
         return;
     }
     for (let i = 0; i < cookies.length; i++) {
-        console.log(`\r\n★★★★★开始执行第${i + 1}个账号,共${cookies.length}个账号★★★★★`);
+        console.log(`\n★★★★★开始执行第${i + 1}个账号,共${cookies.length}个账号★★★★★`);
         thiscookie = cookies[i];
         if (!thiscookie.trim()) continue;
 
@@ -43,9 +41,6 @@ let cityid = Math.round(Math.random() * (1500 - 1000) + 1000);
             }
         });
         deviceid = jsonlist.deviceid_pdj_jd;
-
-        await userinfo();
-        await $.wait(1000);
 
         let tslist = await taskList();
 
@@ -72,28 +67,6 @@ let cityid = Math.round(Math.random() * (1500 - 1000) + 1000);
     $.done();
 })
 
-//个人信息
-async function userinfo() {
-    return new Promise(async resolve => {
-        try {
-            let option = urlTask('https://daojia.jd.com/client?_jdrandom=' + Math.round(new Date()) + '&platCode=H5&appName=paidaojia&channel=&appVersion=8.7.6&jdDevice=&functionId=mine%2FgetUserAccountInfo&body=%7B%22refPageSource%22:%22%22,%22fromSource%22:2,%22pageSource%22:%22myinfo%22,%22ref%22:%22%22,%22ctp%22:%22myinfo%22%7D&jda=&traceId=' + deviceid + Math.round(new Date()) + '&deviceToken=' + deviceid + '&deviceId=' + deviceid + '', '')
-
-            $.http.get(option).then(response => {
-                let data = JSON.parse(response.body);
-                if (data.code == 0) {
-                    nickname = data.result.userInfo.userBaseInfo.nickName;
-                    console.log("●●●" + nickname + "●●●");
-                }
-            })
-            resolve();
-
-        } catch (error) {
-            console.log('\n【个人信息】:' + error);
-            resolve();
-        }
-    })
-}
-
 //任务列表
 async function taskList() {
     return new Promise(async resolve => {
@@ -110,6 +83,7 @@ async function taskList() {
             console.log('\n【任务列表】:' + error);
             resolve({});
         }
+
     })
 }
 
@@ -128,7 +102,9 @@ async function sign() {
             console.log('\n【庄园签到】:' + error);
             resolve();
         }
+
     })
+
 }
 
 //浇水
