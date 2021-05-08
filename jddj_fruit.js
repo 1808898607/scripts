68061bf8f3364936c1e8fb51575a5b1b10361921
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-05-08 18:47:34 +0800

    Update jddj_fruit.js

--- jddj_fruit.js
+++ jddj_fruit.js
@@ -4,7 +4,7 @@
 //cookie只要里面的deviceid_pdj_jd=xxx-xxx-xxx;o2o_m_h5_sid=xxx-xxx-xxx关键信息
 //8,11,16整点各运行一次
 //boxjs订阅地址:https://gitee.com/passerby-b/javascript/raw/master/JD/passerby-b.boxjs.json
-// cron "6 8,11,17 * * *" script-path=https://github.com/717785320/JDDJ/blob/main/jddj_fruit.js
+// cron "2 0,8,11,17 * * *" script-path=https://github.com/717785320/JDDJ/blob/main/jddj_fruit.js
 
 let isNotify = true;//是否通知,仅限nodejs
 
