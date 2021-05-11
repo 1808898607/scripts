f7827905c5c8d13acebe1a5a11637689d941557e
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-05-11 14:36:41 +0800

    Update shell_script_mod.sh

--- shell_script_mod.sh
+++ shell_script_mod.sh
@@ -5,24 +5,20 @@
 
 function sj(){
     # https://github.com/monk-coder/dust
-    rm -rf /acoolbook /scripts/sj_*
+    rm -rf /acoolbook /scripts/*.js
     git clone https://github.com/acoolbook/scripts.git /acoolbook
     # 拷贝脚本
-    for jsname in $(find /acoolbook -name "*.js" | grep -vE "\/backup\/"); do cp ${jsname} /scripts/sj_${jsname##*/}; done
+    for jsname in $(find /acoolbook -name "*.js" | grep -vE "\/backup\/"); do cp ${jsname} /scripts/${jsname##*/}; done
     # 匹配js脚本中的cron设置定时任务
     for jsname in $(find /acoolbook -name "*.js" | grep -vE "\/backup\/"); do
         jsnamecron="$(cat $jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
-        test -z "$jsnamecron" || echo "$jsnamecron node /scripts/sj_${jsname##*/} >> /scripts/logs/sj_${jsname##*/}.log 2>&1" >> /scripts/docker/merged_list_file.sh
+        test -z "$jsnamecron" || echo "$jsnamecron node /scripts/${jsname##*/} >> /scripts/logs/${jsname##*/}.log 2>&1" >> /scripts/docker/merged_list_file.sh
     done
 }
 
-#echo "3 0,6 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_dreamFactory.js >> /scripts/logs/jd_dreamFactory.log 2>&1" >> /scripts/docker/merged_list_file.sh
-echo "10 2,15 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/sj_jd_mc.js >> /scripts/logs/sj_jd_mc.js.log 2>&1" >> /scripts/docker/merged_list_file.sh
-echo "20 4,17 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/sj_jd_mc.js >> /scripts/logs/sj_jd_mc.js.log 2>&1" >> /scripts/docker/merged_list_file.sh
-echo "30 6,19 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/sj_jd_mc.js >> /scripts/logs/sj_jd_mc.js.log 2>&1" >> /scripts/docker/merged_list_file.sh
-echo "40 8,21 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/sj_jd_mc.js >> /scripts/logs/sj_jd_mc.js.log 2>&1" >> /scripts/docker/merged_list_file.sh
-echo "50 10,23 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/sj_jd_mc.js >> /scripts/logs/sj_jd_mc.js.log 2>&1" >> /scripts/docker/merged_list_file.sh
-#sed -i 's/jOkIZzWCgGa9NfPuHBSx1A/AkOULcXbUA_8EAPbYLLMgg/g' /scripts/jd_speed_redpocke.js
+
+echo "10 2,15 * * * sleep $((RANDOM % $RANDOM_DELAY_MAX)); node /scripts/jd_mc.js >> /scripts/logs/jd_mc.js.log 2>&1" >> /scripts/docker/merged_list_file.sh
+
 
 #apk add nodejs-current
 #转换win换行符-Linux
@@ -50,3 +46,4 @@ function main(){
 }
 
 main
+
