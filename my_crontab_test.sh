2e2b88651b6213a0e493667e7a260f9dfd5a6b2f
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-05-11 14:49:01 +0800

    Create my_crontab_test.sh

---
+++ my_crontab_test.sh
@@ -0,0 +1,13 @@
+# 东东超市兑换奖品
+0,30 0 * * * node conc /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
+59 23 * * * sleep 58; node conc /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
+59 23 * * * sleep 59; node conc /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
+# 宠汪汪积分兑换京豆
+0 0 * * * node conc /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_reward.log 2>&1
+59 7,15,23 * * * sleep 58; node conc /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_reward.log 2>&1
+59 7,15,23 * * * sleep 59; node conc /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_reward.log 2>&1
+59 7,15,23 * * * sleep 57; node conc /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_reward.log 2>&1
+
+# 京东汽车旅程赛点兑换金豆
+#0 0 * * * node conc /scripts/jd_car_exchange.js >> /scripts/logs/jd_car_exchange.log 2>&1
+#59 7,15 * * * sleep 58; node conc /scripts/jd_car_exchange.js >> /scripts/logs/jd_car_exchange.log 2>&1
