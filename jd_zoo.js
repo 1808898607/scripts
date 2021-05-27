7a3f792d4ea03e7f113a02b92112ab8c33c0f70b
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-05-27 10:43:02 +0800

    Update jd_zoo.js

--- jd_zoo.js
+++ jd_zoo.js
@@ -712,13 +712,27 @@ function zoo_getHomeData(inviteId= "",timeout = 0) {
             //console.log('zoo_getHomeData:' + JSON.stringify(data))
             secretp = data.data.result.homeMainInfo.secretp
             await zoo_collectProduceScore();
+
             //await zoo_pk_doPkSkill("2");
             await zoo_pk_getHomeData('sSKNX-MpqKOJsNu8n5raUJ3-jxczgnLh4wNbl8y9l_Js24U5k2N6iTgafi5LlHQ')
-
+            await zoo_pk_getHomeData('sSKNX-MpqKOJsNu_zMnRAJCWXSWJwGnbvowWVLic4WQpSlF4rGny63Y6xTETxT4')
+            await zoo_pk_getHomeData('sSKNX-MpqKOJsNu_yJKIULCQ44vz4afPXuTWPiHIGYxy9BRLWe77oYSgoBSRbDA')
+            await zoo_pk_getHomeData('sSKNX-MpqKOJsNv759yKX-65J2N9l__jN4dDpHlNWMV6ADtcjkCMICHzRoQb')
+            await zoo_pk_getHomeData('sSKNX-MpqKOr5byxmJzRa4K6iCgCs9LmMNXpTbo')
+            await zoo_pk_getHomeData('sSKNX-MpqKOJsNu_mZneBluwe_DRzs1f90l6Q_p8OVxtoB-JJEErrVU4eHW7e2I')
             //await zoo_pk_assistGroup()
             if (data.data.result.homeMainInfo.raiseInfo.buttonStatus === 1 ) await zoo_raise(1000)
             await zoo_getHomeData('ZXTKT0225KkcRBgY9gDXIEvyxfdfIgFjRWn6-7zx55awQ');
-
+            await zoo_getHomeData('ZXTKT0225KkcR0tL_VDTdkv3kKUCdgFjRWn6-7zx55awQ');
+            await zoo_getHomeData('ZXTKT0225KkcR08QpACBcUv3l6ICfAFjRWn6-7zx55awQ');
+            await zoo_getHomeData('ZXTKT0205KkcA2Bepg-rQl6swo58FjRWn6-7zx55awQ');
+            await zoo_getHomeData('ZXTKT010xvx7SR8e_QFjRWn6-7zx55awQ');
+            await zoo_getHomeData('ZXTKT0225KkcRhtP81DUdhmgx_QOdgFjRWn6-7zx55awQ');
+            await zoo_getHomeData('ZXTKT0205KkcFH17pDO9fmS-8J1xFjRWn6-7zx55awQ');
+            await zoo_getHomeData('ZXTKT0225KkcRh5LpwDTJR_1kPVZdgFjRWn6-7zx55awQ');
+            await zoo_getHomeData('ZXTKT0225KkcRRlL9FXeJhKgkaIOdgFjRWn6-7zx55awQ');
+            await zoo_getHomeData('ZXTKT01076EkRRoR9QFjRWn6-7zx55awQ');
+            await zoo_getHomeData('ZXTKT0225KkcRx4b8lbWJU72wvZZcwFjRWn6-7zx55awQ');
             await zoo_getTaskDetail("","app")
             await zoo_getTaskDetail()
           } else {
