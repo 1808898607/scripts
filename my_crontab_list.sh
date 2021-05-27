f6b6ab1882662eea1c624b1ba796478461ad74a7
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-05-27 14:08:31 +0800

    Update my_crontab_list.sh

--- my_crontab_list.sh
+++ my_crontab_list.sh
@@ -17,7 +17,7 @@
 9 0,22,23 * * * node /scripts/jd_daily_lottery.js >> /scripts/logs/jd_daily_lottery.log 2>&1
 
 1 0-23/6 * * * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
-13 0-23/8 * * * node /scripts/jd_city.js >> /scripts/logs/jd_city.log 2>&1
+1 0-23/8 * * * node /scripts/jd_city.js >> /scripts/logs/jd_city.log 2>&1
 23 0-23/1 * * * node /scripts/jd_zoo.js >> /scripts/logs/jd_zoo.log 2>&1
 
 #3 0-23/1 * * * node /scripts/jd_zoo.js >> /scripts/logs/jd_zoo.log 2>&1
