2173c8979d6200619e7d7d19b521cb5ffbe949cb
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-05-29 16:41:37 +0800

    Update my_crontab_test.sh

--- my_crontab_test.sh
+++ my_crontab_test.sh
@@ -1,7 +1,8 @@
 # 东东超市兑换奖品
 0,30 0 * * * node conc /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
 59 23 * * * sleep 58; node conc /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
-59 23 * * * sleep 59; node conc /scripts/jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
+5 0 * * * node /scripts/jd_syj.js >> /scripts/logs/jd_syj.log 2>&1
+
 # 宠汪汪积分兑换京豆
 #0 0 * * * node  /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_reward.log 2>&1
 5 0 * * * sleep 59; node conc /scripts/jd_carnivalcity.js >> /scripts/logs/jd_sjkhc.log 2>&1
