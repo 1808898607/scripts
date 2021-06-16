aa707212a82da122a7effbec36c338f99e610ca7
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-06-17 01:04:29 +0800

    Update jd_618redpacket.js

--- jd_618redpacket.js
+++ jd_618redpacket.js
@@ -26,7 +26,7 @@ cron "32 0-23/1 * 6 *" script-path=https://raw.githubusercontent.com/Wenmoux/scr
 const $ = new Env('翻翻乐');
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-const money = $.isNode() ? (process.env.Openmoney ? process.env.Openmoney : 0.6) : 0.6
+const money = $.isNode() ? (process.env.Openmoney ? process.env.Openmoney : 0.3) : 0.3
 const randomCount = $.isNode() ? 20 : 5;
 const notify = $.isNode() ? require('./sendNotify') : '';
 let merge = {}
